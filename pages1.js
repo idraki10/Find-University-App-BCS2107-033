852252b79f72a6d31494074b77835cb8da541d6c
idraki10 <117570430+idraki10@users.noreply.github.com>
2022-11-06 18:30:19 +0800

    Add files via upload

---
+++ pages1.js
@@ -0,0 +1,80 @@
+const inputBox = document.querySelector('#input-box')
+const searchBar = document.querySelector('#search')
+const displayUniversities = document.querySelector('.elements')
+const showHide = document.querySelector('.show-hide')
+const closeIcon = document.querySelector('.close-icon')
+const overlay = document.querySelector('.overlay')
+const alertBox = document.querySelector('.details')
+
+
+function getAll(){
+
+    //e.preventDefault();
+    let inputValue = inputBox.value;
+    let html = "";
+
+    axios.get(`http://universities.hipolabs.com/search?name=${inputValue}`)
+        .then(function (response) {
+        console.log(response);
+
+        let results = response.data
+        //console.log(results)
+        
+        if(results.length) { 
+            results.forEach(element => { 
+            html += `
+            <div>
+              <h4 class="school-name">University name:${element.name}</h4>
+              <p class="school-country">Country:${element.country}</p> 
+              <a class="school-site" href = "${element.web_pages[0]}">${element.web_pages[0]}</a> 
+            </div>
+        `;
+        displayUniversities.innerHTML = html;  
+        }) } else if(results.length === 0){
+            showHide.style.display = "block";
+            overlay.style.display = "block";
+
+            html += `
+            <div>
+                <img src="./assets/not-found.png"  id="not-found-photo"/>
+                <h4 class="oops">Oops! <br>Sorry the word you requested is not found</h4>
+            </div>
+            `;
+
+            alertBox.innerHTML = html
+        }
+        }).catch(function (error) {
+             // handle error
+            if(error){
+                showHide.style.display = "block";
+            overlay.style.display = "block";
+           
+            html += `
+            
+            `;
+            alertBox.innerHTML = html
+            }
+
+            console.log(error);
+        })
+        .then(function () {
+        // always executed
+        }); 
+    } 
+    
+    searchBar.addEventListener('click', getAll)
+
+    document.addEventListener("keyup", function(event) {
+        if (event.code === 'Enter') {
+            getAll()
+         inputBox.value = ""
+        }
+    });
+
+    function closeModal(){ 
+        showHide.style.display = "none";
+        overlay.style.display = "none";
+    }
+    closeIcon.addEventListener('click', closeModal)
+   
+
